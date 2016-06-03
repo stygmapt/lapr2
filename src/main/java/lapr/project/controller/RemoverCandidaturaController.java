df0b9b1a6b039659439cdbd2b0af74ad40b89ddd
1120473 <1120473@isep.ipp.pt>
2016-06-03 12:46:02 +0100

    GUI remover candidatura, esqueleto UC 17

---
+++ src/main/java/lapr/project/controller/RemoverCandidaturaController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import lapr.project.model.CentroExposicoes;
+
+/**
+ *
+ * @author Joao Sousa
+ */
+public class RemoverCandidaturaController {
+    
+    private CentroExposicoes centro;
+    
+    public RemoverCandidaturaController(CentroExposicoes c){
+        this.centro = c;
+    }
+}
