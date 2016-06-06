3ab4cbdeada7a0988d6696ff6462ee5b79af5442
1141330 <1141330@isep.ipp.pt>
2016-06-06 09:23:03 +0100

    Implementacion of Candidaturas State's.

--- src/main/java/lapr/project/model/Candidatura.java
+++ src/main/java/lapr/project/model/Candidatura.java
@@ -6,104 +6,112 @@
 package lapr.project.model;
 
 import lapr.project.states.CandidaturaState;
-import lapr.project.states.CandidaturaStateNaoAvaliada;
+import lapr.project.states.CandidaturaStateEmSubmissao;
 
 /**
  *
  * @author Gabriel
  */
 public class Candidatura {
-    
-    private Utilizador representante;
-    private String nome_Empresa;
-    private String morada_Empresa;
-    private int telemovel_Empresa;
-    private int qtd_convites;
-    private float area_Empresa;
-    private ListaProdutos lista_produtos;
-    private CandidaturaState estado_candidatura;
 
-    public Candidatura() {
-    }
+	private Utilizador representante;
+	private String nome_Empresa;
+	private String morada_Empresa;
+	private int telemovel_Empresa;
+	private int qtd_convites;
+	private float area_Empresa;
+	private ListaProdutos lista_produtos;
+	private CandidaturaState estado_candidatura;
 
-    public Candidatura(Utilizador representante, String nome_Empresa, String morada_Empresa, int telemovel_Empresa, int qtd_convites, float area_Empresa, ListaProdutos lista_produtos) {
-        this.representante = representante;
-        this.nome_Empresa = nome_Empresa;
-        this.morada_Empresa = morada_Empresa;
-        this.telemovel_Empresa = telemovel_Empresa;
-        this.qtd_convites = qtd_convites;
-        this.area_Empresa = area_Empresa;
-        this.lista_produtos = lista_produtos;
-        this.estado_candidatura = new CandidaturaStateNaoAvaliada();
-    }
+	public Candidatura() {
+	}
 
-    public Utilizador getRepresentante() {
-        return representante;
-    }
+	public Candidatura(Utilizador representante, String nome_Empresa,
+					   String morada_Empresa, int telemovel_Empresa,
+					   int qtd_convites, float area_Empresa,
+					   ListaProdutos lista_produtos) {
+		this.representante = representante;
+		this.nome_Empresa = nome_Empresa;
+		this.morada_Empresa = morada_Empresa;
+		this.telemovel_Empresa = telemovel_Empresa;
+		this.qtd_convites = qtd_convites;
+		this.area_Empresa = area_Empresa;
+		this.lista_produtos = lista_produtos;
+		this.estado_candidatura = new CandidaturaStateEmSubmissao(this);
+	}
 
-    public void setRepresentante(Utilizador representante) {
-        this.representante = representante;
-    }
+	public Utilizador getRepresentante() {
+		return representante;
+	}
 
-    public String getNome_Empresa() {
-        return nome_Empresa;
-    }
+	public void setRepresentante(Utilizador representante) {
+		this.representante = representante;
+	}
 
-    public void setNome_Empresa(String nome_Empresa) {
-        this.nome_Empresa = nome_Empresa;
-    }
+	public String getNome_Empresa() {
+		return nome_Empresa;
+	}
 
-    public String getMorada_Empresa() {
-        return morada_Empresa;
-    }
+	public void setNome_Empresa(String nome_Empresa) {
+		this.nome_Empresa = nome_Empresa;
+	}
 
-    public void setMorada_Empresa(String morada_Empresa) {
-        this.morada_Empresa = morada_Empresa;
-    }
+	public String getMorada_Empresa() {
+		return morada_Empresa;
+	}
 
-    public int getTelemovel_Empresa() {
-        return telemovel_Empresa;
-    }
+	public void setMorada_Empresa(String morada_Empresa) {
+		this.morada_Empresa = morada_Empresa;
+	}
 
-    public void setTelemovel_Empresa(int telemovel_Empresa) {
-        this.telemovel_Empresa = telemovel_Empresa;
-    }
+	public int getTelemovel_Empresa() {
+		return telemovel_Empresa;
+	}
 
-    public int getQtd_convites() {
-        return qtd_convites;
-    }
+	public void setTelemovel_Empresa(int telemovel_Empresa) {
+		this.telemovel_Empresa = telemovel_Empresa;
+	}
 
-    public void setQtd_convites(int qtd_convites) {
-        this.qtd_convites = qtd_convites;
-    }
+	public int getQtd_convites() {
+		return qtd_convites;
+	}
 
-    public float getArea_Empresa() {
-        return area_Empresa;
-    }
+	public void setQtd_convites(int qtd_convites) {
+		this.qtd_convites = qtd_convites;
+	}
 
-    public void setArea_Empresa(float area_Empresa) {
-        this.area_Empresa = area_Empresa;
-    }
+	public float getArea_Empresa() {
+		return area_Empresa;
+	}
 
-    public ListaProdutos getLista_produtos() {
-        return lista_produtos;
-    }
+	public void setArea_Empresa(float area_Empresa) {
+		this.area_Empresa = area_Empresa;
+	}
 
-    public void setLista_produtos(ListaProdutos lista_produtos) {
-        this.lista_produtos = lista_produtos;
-    }
-    
-    /**
-     *
-     * @return
-     */
-    @Override
-    public String toString(){
-        return "Candidatura:\tNome da Empresa: " + this.nome_Empresa + "\tRepresentante: "+ this.representante.toString();
-    }
+	public ListaProdutos getLista_produtos() {
+		return lista_produtos;
+	}
 
-    public boolean valida() {
-        return true;
-    }
-    
+	public void setLista_produtos(ListaProdutos lista_produtos) {
+		this.lista_produtos = lista_produtos;
+	}
+
+	public boolean isInSubmissao() {
+		return this.estado_candidatura.setEmSubmissao();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	@Override
+	public String toString() {
+		return "Candidatura:\tNome da Empresa: " + this.nome_Empresa + "\tRepresentante: " + this.representante.
+			toString();
+	}
+
+	public boolean valida() {
+		return true;
+	}
+
 }
